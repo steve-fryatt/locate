fbe02500cea66d7ba539db10eda314dd0a87d0f4
Steve Fryatt <info@stevefryatt.org.uk>
2012-02-12 18:51:01 +0000

    Untested search path handling code.

--- src/search.c
+++ src/search.c
@@ -37,23 +37,47 @@
 #include "results.h"
 
 
-#define SEARCH_ALLOC_STACK 20							/**< The directory depth in which to allocate stack space.	*/
+#define SEARCH_ALLOC_STACK 20							/**< The directory depth in which to allocate stack space.		*/
 
-#define SEARCH_MAX_FILENAME 256							/**< The maximum length of a file (object) name.		*/
+#define SEARCH_MAX_FILENAME 256							/**< The maximum length of a file (object) name.			*/
+#define SEARCH_BLOCK_SIZE 4096							/**< The amount of memory to allocate to OS_GBPB.			*/
 
+#define SEARCH_NULL 0xffffffff							/**< 'NULL' value for use with the unsigned flex block offsets.		*/
+
+/* A data structure to hold the search stack. */
 
 struct search_stack {
-	char			filename[SEARCH_MAX_FILENAME];			/**< The filename of the current directory.			*/
+	char			filename[SEARCH_MAX_FILENAME];			/**< The filename of the current directory.				*/
+	bits			info[SEARCH_BLOCK_SIZE];			/**< Block for OS_GBPB 10.						*/
+
+	int			read;
+	int			next;
 };
+
+/* A data structure defining a search. */
 
 struct search_block {
-	struct file_block	*file;						/**< The file to which the search belongs.			*/
-	struct results_window	*results;					/**< Results module to output results to.			*/
+	struct file_block	*file;						/**< The file to which the search belongs.				*/
+	struct results_window	*results;					/**< Results module to output results to.				*/
 
-	struct search_stack	*stack;						/**< The search stack.						*/
+	osbool			active;						/**< TRUE if the search is active; else FALSE.				*/
+
+	int			path_count;					/**< The number of search paths remaining to use.			*/
+	char			*paths;						/**< Line containing the full set of search paths.			*/
+	char			*path[];					/**< Index to each of the search paths.					*/
+
+	struct search_stack	*stack;						/**< The search stack.							*/
+	unsigned		stack_level;					/**< The current stack level.						*/
+	unsigned		stack_size;					/**< The amount of stack levels currently claimed.			*/
 };
 
 
+/* Global variables. */
+
+
+/* Local function prototypes. */
+
+static unsigned	search_add_stack(struct search_block *search);
 
 /**
  * Create a new search.
@@ -61,15 +85,27 @@ struct search_block {
  * \param *file			The file block to which the search belongs.
  * \param *results		The results window to which output should be
  *				directed.
+ * \param *path			The path(s) to search, comma-separated.
  * \return			The search handle, or NULL on failure.
  */
 
-struct search_block *search_create(struct file_block *file, struct results_window *results)
+struct search_block *search_create(struct file_block *file, struct results_window *results, char *path)
 {
 	struct search_block	*new = NULL;
 	osbool			mem_ok = TRUE;
+	int			i, paths;
 
-		/* Allocate all the memory that we require. */
+	/* Test and count the supplied paths. */
+
+	if (path == NULL)
+		return NULL;
+
+	paths = 0;
+	for (i = 0; path[i] != '\0'; i++)
+		if (path[i] == ',')
+			paths++;
+
+	/* Allocate all the memory that we require. */
 
 	new = heap_alloc(sizeof(struct search_block));
 	if (new == NULL)
@@ -77,6 +113,14 @@ struct search_block *search_create(struct file_block *file, struct results_windo
 
 	if (mem_ok) {
 		new->stack = NULL;
+		new->paths = NULL;
+		new->path = NULL;
+
+		if ((new->paths = heap_strdup(path)) == NULL)
+			mem_ok = FALSE;
+
+		if ((new->paths = heap_alloc(paths * sizeof(char *))) == NULL)
+			mem_ok = FALSE;
 
 		if (flex_alloc((flex_ptr) &(new->stack), SEARCH_ALLOC_STACK * sizeof(struct search_stack)) == 0)
 			mem_ok = FALSE;
@@ -90,6 +134,12 @@ struct search_block *search_create(struct file_block *file, struct results_windo
 		if (new != NULL && new->stack != NULL)
 			flex_free((flex_ptr) &(new->stack));
 
+		if (new != NULL && new->path != NULL)
+			heap_free(new->path);
+
+		if (new != NULL && new->paths != NULL)
+			heap_free(new->paths);
+
 		if (new != NULL)
 			heap_free(new);
 
@@ -97,8 +147,29 @@ struct search_block *search_create(struct file_block *file, struct results_windo
 		return NULL;
 	}
 
+
+	/* Initialise the search contents. */
+
 	new->file = file;
 	new->results = results;
+
+	new->active = FALSE
+
+	new->stack_size = SEARCH_ALLOC_STACK;
+	new->stack_level = 0;
+
+	new->path_count = paths;
+
+	/* Split the path list into separate paths. */
+
+	new->path[--paths] = new->paths;
+
+	for (i = 0; new->paths[i] != '\0'; i++) {
+		if (new->paths[i] == ',' && paths > 0) {
+			new->path[--paths] = new->paths + i + 1;
+			new->paths[i] = '\0';
+		}
+	}
 
 	return new;
 }
@@ -116,6 +187,64 @@ void search_destroy(struct search_block *search)
 		return;
 
 	flex_free((flex_ptr) &(search->stack));
+	heap_free(new->path);
+	heap_free(new->paths);
 
 	heap_free(search);
 }
+
+
+
+osbool search_poll(struct search_block *search, os_t end_time)
+{
+	os_error	*error;
+	osbool		done = FALSE
+	int		read, next;
+
+	while (!done && os_read_monotomic_time() < end_time) {
+		error = xosgbpb_dir_entries_info(path, 1000, search->stack[search->stack_level].next,
+				SEARCH_BLOCK_SIZE, NULL, &read, &next);
+
+
+	}
+}
+
+
+
+
+/**
+ * Claim a new line from the search stack, allocating more memory if required,
+ * set it up and return its offset.
+ *
+ * \param *handle		The handle of the search.
+ * \return			The offset of the new entry, or SEARCH_NULL.
+ */
+
+static unsigned search_add_stack(struct search_block *search)
+{
+	unsigned offset;
+
+	if (search == NULL)
+		return SEARCH_NULL;
+
+	/* Make sure that there is enough space in the block to take the new
+	 * level, allocating more if required.
+	 */
+
+	if (search->stack_level >= search->stack_size) {
+		if (flex_extend((flex_ptr) &(search->stack), (search->stack_size + SEARCH_ALLOC_STACK) * sizeof(struct search_stack)) == 0)
+			return SEARCH_NULL;
+
+		search->stack_size += SEARCH_ALLOC_STACK;
+	}
+
+	/* Get the new level and initialise it. */
+
+	offset = search->stack_level++;
+
+	search->stack[offset].read = 0;
+	search->stack[offset].next = 0;
+
+	return offset;
+}
+
