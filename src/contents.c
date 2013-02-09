a7014e4c6dd2706d26bcd3ea8ccf59f6694b44ad
Steve Fryatt <info@stevefryatt.org.uk>
2013-02-09 19:30:30 +0000

    Begin refactoring of data load code.

--- src/contents.c
+++ src/contents.c
@@ -38,6 +38,7 @@
 
 /* OSLib header files */
 
+#include "oslib/osargs.h"
 #include "oslib/osfile.h"
 #include "oslib/osfind.h"
 #include "oslib/osgbpb.h"
@@ -77,11 +78,17 @@ struct contents_block {
 
 	char				*file;					/**< Flex block containing the file data or a subset of it.	*/
 	size_t				file_block_size;			/**< The number of bytes allocated to the file block.		*/
-	unsigned			file_offset;				/**< Offset from the start of the file to the file data.	*/
-	unsigned			file_length;				/**< The number of bytes of file data.				*/
+
+	int				file_length;				/**< The number of bytes of file data on disc.			*/
+	int				file_offset;				/**< File ptr for the start of the data in memory.		*/
 
 	unsigned			count;
 };
+
+
+
+static osbool	contents_load_file_chunk(struct contents_block *handle, int position);
+static char	contents_get_byte(struct contents_block *handle, unsigned byte);
 
 
 /**
@@ -176,11 +183,8 @@ void contents_destroy(struct contents_block *handle)
 
 osbool contents_add_file(struct contents_block *handle, unsigned key)
 {
-	size_t		filename_length, block_length;
+	size_t		filename_length;
 	os_error	*error;
-	int		size;
-	os_fw		input_handle;
-	int		unread;
 
 	if (handle == NULL || key == OBJDB_NULL_KEY)
 		return FALSE;
@@ -206,39 +210,26 @@ osbool contents_add_file(struct contents_block *handle, unsigned key)
 	 * available space.
 	 */
 
-	error = xosfile_read_no_path(handle->filename, NULL, NULL, NULL, &size, NULL);
+	error = xosfile_read_no_path(handle->filename, NULL, NULL, NULL, &handle->file_length, NULL);
 	if (error != NULL) {
 		results_add_error(handle->results, error->errmess, handle->filename);
+		handle->file_length = 0;
 		return FALSE;
 	}
 
-	block_length = (size > handle->file_block_size) ? handle->file_block_size : size;
+	/* Load the first chunk of data from the file. */
 
-	debug_printf("Filename: %s, size: %d, block length: %d", handle->filename, size, block_length);
+	contents_load_file_chunk(handle, 0);
+
+
+
+
+	//block_length = (size > handle->file_block_size) ? handle->file_block_size : size;
+
+	//debug_printf("Filename: %s, size: %d, block length: %d", handle->filename, size, block_length);
 
 	/* Load the first block of the file into memory. */
 
-	error = xosfind_openinw(osfind_NO_PATH | osfind_ERROR_IF_DIR, handle->filename, NULL, &input_handle);
-	if (error != NULL || input_handle == 0) {
-		results_add_error(handle->results, (error != NULL) ? error->errmess : "", handle->filename);
-		return FALSE;
-	}
-
-	error = xosgbpb_readw(input_handle, (byte *) handle->file, block_length, &unread);
-	if (error != NULL) {
-		results_add_error(handle->results, error->errmess, handle->filename);
-		return FALSE;
-	} else {
-		handle->file_length = block_length;
-		*(handle->file + 25) = '\0';
-		debug_printf("Loaded data: '%s;", handle->file);
-	}
-
-	error = xosfind_close(input_handle);
-	if (error != NULL) {
-		results_add_error(handle->results, error->errmess, handle->filename);
-		return FALSE;
-	}
 
 	results_add_file(handle->results, key);
 
@@ -272,3 +263,84 @@ osbool contents_poll(struct contents_block *handle, os_t end_time, osbool *match
 	return (handle->count == 0) ? TRUE : FALSE;
 }
 
+
+
+static osbool contents_load_file_chunk(struct contents_block *handle, int position)
+{
+	int		extent, ptr, bytes, unread;
+	os_fw		file;
+	os_error	*error;
+
+
+	if (handle == NULL)
+		return FALSE;
+
+	/* Open the file. */
+
+	error = xosfind_openinw(osfind_NO_PATH | osfind_ERROR_IF_DIR, handle->filename, NULL, &file);
+	if (error != NULL || file == 0) {
+		results_add_error(handle->results, (error != NULL) ? error->errmess : "", handle->filename);
+		return FALSE;
+	}
+
+	/* Get the file's extent. */
+
+	error = xosargs_read_extw(file, &extent);
+	if (error != NULL) {
+		results_add_error(handle->results, error->errmess, handle->filename);
+		return FALSE;
+	}
+
+	/* If the file extent isn't the same as it was at the start, get out. */
+
+	if (extent != handle->file_length) {
+		results_add_error(handle->results, "File changed!", handle->filename);
+		error = xosfind_close(file);
+		if (error != NULL)
+			results_add_error(handle->results, error->errmess, handle->filename);
+		return FALSE;
+	}
+
+	/* If the file completely fits into the block, then always load it from
+	 * the start. Otherwise, make sure that the position means that the blocl
+	 * is completely full.
+	 */
+
+	if (extent > handle->file_block_size) {
+		ptr = (extent - position > handle->file_block_size) ? position : extent - position > handle->file_block_size;
+		bytes = handle->file_block_size;
+	} else {
+		ptr = 0;
+		bytes = extent;
+	}
+
+	error = xosgbpb_read_atw(file, (byte *) handle->file, bytes, ptr, &unread);
+	if (error != NULL || unread != 0) {
+		results_add_error(handle->results, (error != NULL) ? error->errmess : "", handle->filename);
+		return FALSE;
+	} else {
+		handle->file_offset = ptr;
+
+//		*(handle->file + 25) = '\0';
+//		debug_printf("Loaded data: '%s;", handle->file);
+	}
+
+	error = xosfind_close(file);
+	if (error != NULL) {
+		results_add_error(handle->results, error->errmess, handle->filename);
+		return FALSE;
+	}
+
+	return TRUE;
+}
+
+
+
+static char contents_get_byte(struct contents_block *handle, unsigned byte)
+{
+	if (handle == NULL)
+		return 0;
+
+
+	return 0;
+}
