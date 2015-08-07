cee74c020054cd8855a189682468622f11972d82
Steve Fryatt <info@stevefryatt.org.uk>
2015-08-07 22:56:00 +0000

    Comment the importance of not deleting database objects relating to OS_GBPB errors.

--- src/search.c
+++ src/search.c
@@ -1,4 +1,4 @@
-/* Copyright 2012, Stephen Fryatt (info@stevefryatt.org.uk)
+/* Copyright 2012-2015, Stephen Fryatt (info@stevefryatt.org.uk)
  *
  * This file is part of Locate:
  *
@@ -785,7 +785,11 @@ static osbool search_poll(struct search_block *search, os_t end_time)
 			}
 
 			/* Handle any errors thrown by the OS_GBPB call by dropping back out of
-			 * the current directory.
+			 * the current directory. Because we're adding an error with a database
+			 * key, it's important that we don't continue out of the current loop and
+			 * don't call objdb_delete_last_key() in the usual way -- otherwise, the
+			 * key that's linked to the error will probably get removed from the
+			 * object database.
 			 */
 
 			if (error != NULL) {
